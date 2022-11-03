7b12797bbd52ad695b051e58aac3cad225b23439
kovacs.gabor5224 <kovac@DESKTOP-QGIEMMT>
2022-11-03 14:44:47 +0100

    Added new class for user handling

---
+++ WindowsFormsApp1/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    internal class User
+    {
+        private bool _isLogged;
+        public List<string> Exceptions = new List<string>();
+
+        public bool IsUserLogged 
+        { 
+            get { return _isLogged; }
+            private set
+            {
+                _isLogged = Properties.Settings.Default.userIsLogged;
+            }
+        }
+
+        public void Login(string password)
+        {
+            if (password == "7GJJrbEd+)eXmD@B")
+                Properties.Settings.Default.userIsLogged = true;
+            else
+                this.Exceptions.Add("Hibás jelszót adtál meg!");
+
+            if (this.Exceptions.Any())
+                throw new Exception("Hiba!");
+        }
+    }
+}
