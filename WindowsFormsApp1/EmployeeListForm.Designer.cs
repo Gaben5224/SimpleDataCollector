ff45b2a43b32c037ff61c1f7f2edf8f2aca3f045
kovacs.gabor5224 <kovac@DESKTOP-QGIEMMT>
2022-11-07 22:00:47 +0100

    Finish new UI.

--- WindowsFormsApp1/EmployeeListForm.Designer.cs
+++ WindowsFormsApp1/EmployeeListForm.Designer.cs
@@ -29,26 +29,26 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.panel5 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -61,6 +61,55 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(403, 235);
             this.panel1.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.textBox2);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Controls.Add(this.textBox3);
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Controls.Add(this.button1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 101);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(403, 130);
+            this.panel5.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(166, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(23, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Dolgozó kártyaszáma";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(166, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(30, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Dolgozó munkaköre";
             // 
             // button1
             // 
@@ -75,44 +124,47 @@
             this.button1.TabIndex = 11;
             this.button1.Text = "Mentés";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label4
+            // panel4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(30, 51);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(130, 18);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Dolgozó munkaköre";
+            this.panel4.Controls.Add(this.comboBox1);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 67);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(403, 34);
+            this.panel4.TabIndex = 14;
             // 
-            // textBox3
+            // comboBox1
             // 
-            this.textBox3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox3.Location = new System.Drawing.Point(166, 48);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(188, 26);
-            this.textBox3.TabIndex = 9;
-            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.comboBox1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(166, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(188, 26);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // label2
+            // label1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(23, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 18);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Dolgozó kártyaszáma";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(68, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Dolgozó neve";
             // 
-            // textBox2
+            // panel3
             // 
-            this.textBox2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox2.Location = new System.Drawing.Point(166, 6);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(188, 26);
-            this.textBox2.TabIndex = 7;
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 33);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(403, 34);
+            this.panel3.TabIndex = 13;
             // 
             // label3
             // 
@@ -131,7 +183,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(188, 26);
             this.textBox1.TabIndex = 5;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // panel2
             // 
@@ -142,6 +193,18 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(403, 33);
             this.panel2.TabIndex = 12;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox2.Location = new System.Drawing.Point(231, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(165, 22);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Új dolgozó hozzáadása";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox1
             // 
@@ -157,70 +220,6 @@
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox2.Location = new System.Drawing.Point(231, 3);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(165, 22);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Új dolgozó hozzáadása";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.label3);
-            this.panel3.Controls.Add(this.textBox1);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 33);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(403, 34);
-            this.panel3.TabIndex = 13;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.comboBox1);
-            this.panel4.Controls.Add(this.label1);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 67);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(403, 34);
-            this.panel4.TabIndex = 14;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(68, 6);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 18);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Dolgozó neve";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(166, 3);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(188, 26);
-            this.comboBox1.TabIndex = 7;
-            // 
-            // panel5
-            // 
-            this.panel5.Controls.Add(this.textBox2);
-            this.panel5.Controls.Add(this.label2);
-            this.panel5.Controls.Add(this.textBox3);
-            this.panel5.Controls.Add(this.label4);
-            this.panel5.Controls.Add(this.button1);
-            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel5.Location = new System.Drawing.Point(0, 101);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(403, 130);
-            this.panel5.TabIndex = 15;
-            // 
             // EmployeeListForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -229,16 +228,17 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "EmployeeListForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EmployeeListForm";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
--- WindowsFormsApp1/EmployeeListForm.cs
+++ WindowsFormsApp1/EmployeeListForm.cs
@@ -15,100 +15,22 @@ namespace DataCollector
     public partial class EmployeeListForm : Form
     {
         private Employee _employee = new Employee();
-        private int _numberOfEmployees;
+        private bool _isNewEmployee;
         public EmployeeListForm()
         {
+            var names = _employee.GetAllEmployeesName();
             InitializeComponent();
             this.panel3.Enabled = false;
-            this.panel3.Visible = false; 
-            //SetStyleDataGridView1();
-            //this.FillEmployeeList();
-        }
+            this.panel3.Visible = false;
+            this.comboBox1.Items.Add("");
 
-        private void FillEmployeeList()
-        {
-            //try
-            //{
-            //    var allEmployeesData = _employee.GetAllEmployeesData();
-            //    var rowsCount = allEmployeesData.GetLength(0);
-            //    var columnCount = allEmployeesData.GetLength(1);
-            //    this._numberOfEmployees = rowsCount;
-
-            //    for (int i = 0; i < rowsCount; i++)
-            //    {
-            //        for (int j = 0; j < columnCount; j++)
-            //        {
-            //            if (j == 0)
-            //                this.dataGridView1.Rows.Add();
-
-            //            this.dataGridView1.Rows[i].Cells[j].Value = allEmployeesData.GetValue(i, j);
-            //        }
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    var exceptions = this._employee.Exceptions;
-
-            //    foreach(string exception in exceptions)
-            //    {
-            //        MessageBox.Show(this, exception, ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //}
             
+            foreach(var name in names)
+            {
+                this.comboBox1.Items.Add(name);
+            }
         }
 
-        private void SetStyleDataGridView1()
-        {
-            //dataGridView1.DefaultCellStyle.Font = new Font("Calibri", 11.0f);
-            //dataGridView1.DefaultCellStyle.SelectionBackColor = Color.LightGray;
-            //dataGridView1.DefaultCellStyle.SelectionForeColor = Color.Black;
-            //dataGridView1.RowTemplate.Height = 30;
-            ////dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
-            //dataGridView1.EnableHeadersVisualStyles = false;
-            //dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0, 102, 204);
-            //dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-            //dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Calibri", 12.0f);
-            //dataGridView1.ColumnHeadersDefaultCellStyle.Padding = new Padding(0, 5, 0, 5);
-            //dataGridView1.AdvancedColumnHeadersBorderStyle.All = DataGridViewAdvancedCellBorderStyle.Single;
-            //dataGridView1.AdvancedCellBorderStyle.All = DataGridViewAdvancedCellBorderStyle.Single;
-
-            ////DataGridViewButtonColumn style = (DataGridViewButtonColumn)dataGridView1.Columns["EditButton"];
-            ////style.FlatStyle = FlatStyle.Flat;
-            ////style.DefaultCellStyle.BackColor = Color.Green;
-
-        }
-
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            //var index = e.RowIndex;
-
-            //if (_numberOfEmployees > index)
-            //{
-            //    var nodeID = dataGridView1.Rows[index].Cells[0].Value;
-            //    var id = dataGridView1.Rows[index].Cells[1].Value;
-            //    var name = dataGridView1.Rows[index].Cells[2].Value;
-            //    var jobTitle = dataGridView1.Rows[index].Cells[3].Value;
-
-            //    this.textBox1.Text = name.ToString();
-            //    this.textBox2.Text = id.ToString();
-            //    this.textBox3.Text = jobTitle.ToString();
-
-            //    this.button1.Text = "Szerkesztés";
-
-            //    _employee.EditEmployee(Convert.ToInt32(nodeID), name.ToString(), Convert.ToInt32(id), jobTitle.ToString());
-
-            //    this.dataGridView1.Refresh();
-            //}
-            //else
-            //{
-            //    this.textBox1.ResetText();
-            //    this.textBox2.ResetText();
-            //    this.textBox3.ResetText();
-
-            //    this.button1.Text = "Hozzáadás";
-            //}
-            
-        }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
@@ -119,6 +41,8 @@ namespace DataCollector
                 this.panel3.Visible = false;
                 this.panel4.Enabled = true;
                 this.panel4.Visible = true;
+                this.textBox1.ResetText();
+                this._isNewEmployee = false;
             }
             else
             {
@@ -127,6 +51,10 @@ namespace DataCollector
                 this.panel3.Visible = true;
                 this.panel4.Enabled = false;
                 this.panel4.Visible = false;
+                this.textBox2.ResetText();
+                this.textBox3.ResetText();
+                this.comboBox1.ResetText();
+                this._isNewEmployee = true;
             }  
         }
 
@@ -136,6 +64,39 @@ namespace DataCollector
                 this.checkBox1.Checked = false;
             else
                 this.checkBox1.Checked = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > 0)
+            {
+                _employee.GetEmployeeDataByIndex(comboBox1.SelectedIndex - 1);
+
+                textBox2.Text = _employee.Id.ToString();
+                textBox3.Text = _employee.JobTitle;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!this._isNewEmployee)
+            {
+                try
+                {
+                    _employee.EditEmployee(_employee.NodeID, comboBox1.Text, Convert.ToInt32(textBox2.Text), textBox3.Text);
+
+                    MessageBox.Show("Sikeres mentés!", "OK");
+                }
+                catch (Exception ex)
+                {
+                    var exceptions = _employee.Exceptions;
+
+                    foreach(var exception in exceptions)
+                    {
+                        MessageBox.Show(this, exception, ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
     }
 }
